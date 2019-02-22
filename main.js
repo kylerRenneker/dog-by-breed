d37d722013bb552a21131e57880322a521d1b7e4
Kyler <Krenneker16@gmail.com>
2019-02-21 19:02:18 -0500

    fixed error catch

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ function getDogImage(breed){
     fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
         .then(response => response.json())
         .then(responseJson => displayResults(responseJson))
-    .catch(error => alert('Woops! Something went wrong, try again!'));
-}
+    .catch(error => alert('Woops! Could not find the breed or something went wrong, try again!'));
+};
 
 function displayResults(responseJson){
     console.log(responseJson);
     if(responseJson.status === 'success'){
     $('.js-dog-results').append(`<img src="${responseJson.message}">`);
+    }
+    else {
+        return error;
     }
 }
 
